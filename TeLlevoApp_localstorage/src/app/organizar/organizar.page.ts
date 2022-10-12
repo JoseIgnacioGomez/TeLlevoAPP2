251c73bef1a23d4d9e6ad8f48f3fd8d20610e5e2
ReinaldoFernandezC <101837525+ReinaldoFernandezC@users.noreply.github.com>
2022-10-12 15:18:37 -0300

    Add files via upload

---
+++ TeLlevoApp_localstorage/src/app/organizar/organizar.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organizar',
+  templateUrl: './organizar.page.html',
+  styleUrls: ['./organizar.page.scss'],
+})
+export class OrganizarPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
