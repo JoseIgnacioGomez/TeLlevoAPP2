e18effa10591e3f521556330232f466a1037f9d7
ReinaldoFernandezC <101837525+ReinaldoFernandezC@users.noreply.github.com>
2022-10-12 15:17:23 -0300

    Add files via upload

---
+++ TeLlevoApp_localstorage/src/app/services/bd-local.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BdLocalService {
+
+  
+
+  constructor() { }
+}
