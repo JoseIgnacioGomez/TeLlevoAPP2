193e9f6edc119918128c23bcac955db0323d5bd2
JoseIgnacioGomez <101642772+JoseIgnacioGomez@users.noreply.github.com>
2022-10-17 16:59:08 -0300

    Add files via upload

--- TeLlevoApp_conGuardfuncional2/src/app/app.module.ts
+++ TeLlevoApp_conGuardfuncional2/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { MapsModule } from './maps/maps.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MapsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
